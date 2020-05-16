2bff497b3b97de8699278d4bb65d3bca57c0cb5a
clockvoid <hidenari.t1997nov@gmail.com>
2020-05-16 21:43:33 +0900

    add: breadcrumb on index page

--- src/Html/Index.hs
+++ src/Html/Index.hs
@@ -4,7 +4,11 @@ module Html.Index
   ( indexPage
   ) where
 
+import Data.Text
 import Lucid
+
+imgClockvoid :: Text
+imgClockvoid = "https://i.imgur.com/rLs5sRT.png"
 
 indexPage :: (FilePath, Html ())
 indexPage = (indexPath, indexHtml)
@@ -22,4 +26,16 @@ indexHtml = do
       meta_ [httpEquiv_ "x-ua-compatible", content_ "ie=edge"]
       meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
       link_ [rel_ "stylesheet", href_ "/css/mystyles.css"]
+    body_ $
+      section_ [class_ "hero is-primary is-fullheight"] $
+        div_ [class_ "hero-body"] $
+          div_ [class_ "container has-text-centered"] $ do
+            figure_ [class_ "image is-128x128", style_ "margin: 0 auto 1.5rem auto"] $
+              img_ [class_ "is-rounded", src_ imgClockvoid]
+            h1_ [class_ "title is-size-1-desktop is-size-2-touch"] "clockvoid"
+            nav_ [class_ "breadcrumb is-centered has-bullet-separator"] $
+              ul_ $ do
+                li_ $ a_ [class_ "navbar-item"] "About"
+                li_ $ a_ [class_ "navbar-item"] "Blog"
+                li_ $ a_ [class_ "navbar-item"] "Works"
 
--- src/Site/Index.hs
+++ src/Site/Index.hs
@@ -11,14 +11,11 @@ index :: Rules ()
 index = match indexPattern $ do
     route $ constRoute "index.html"
     compile $ do
-        posts <- recentFirst =<< loadAll postPattern
         let indexCtx =
-                listField "posts" postCtx (return posts) `mappend`
-                constField "title" "Home"                `mappend`
+                constField "title" "Home" `mappend`
                 defaultContext
 
         getResourceBody
             >>= applyAsTemplate indexCtx
-            >>= loadAndApplyTemplate defaultTemplate indexCtx
             >>= relativizeUrls
 
