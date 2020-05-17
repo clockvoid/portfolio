8e4802f19d59d72a5001432b7f0d264ca5d58134
clockvoid <hidenari.t1997nov@gmail.com>
2020-05-18 02:02:19 +0900

    fix: pandoc compiler to optimize bulma

--- src/Site/Posts.hs
+++ src/Site/Posts.hs
@@ -6,11 +6,12 @@ import Data.Monoid (mappend)
 import Hakyll
 
 import Site.Lib
+import Site.Pandoc (myPandocCompiler)
 
 posts :: Rules ()
 posts = match postPattern $ do
     route $ setExtension "html"
-    compile $ pandocCompiler
+    compile $ myPandocCompiler
         >>= loadAndApplyTemplate postTemplate postCtx
         >>= loadAndApplyTemplate articleTemplate postCtx
         >>= relativizeUrls
