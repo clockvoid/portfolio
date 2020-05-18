d218cee3d943afc106f255cb8d9f6a05d0aaabeb
clockvoid <hidenari.t1997nov@gmail.com>
2020-05-18 23:25:00 +0900

    fix: margin top of title

--- src/Html/BlogPost.hs
+++ src/Html/BlogPost.hs
@@ -15,7 +15,7 @@ blogPostPath = "templates/blog-post.html"
 
 blogPostHtml :: Html ()
 blogPostHtml = do
-  h1_ [class_ "title is-1", style_ "margin-top: 1rem"] "$title$"
+  h1_ [class_ "title is-1"] "$title$"
   p_ [class_ "date", style_ "margin-bottom: 1rem"] "Posted on $date$ $if(updated-date)$ , Updated on $updated-date$ $endif$"
   p_ [style_ "margin-bottom: 1rem"] "$if(summary)$ $summary$ $endif$"
   article_ [] "$body$"
