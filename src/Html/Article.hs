6ba0d95b28e0139054340b83a28b9ef26dc13e14
clockvoid <hidenari.t1997nov@gmail.com>
2020-05-22 00:47:15 +0900

    fix: twitter card

--- src/Html/Article.hs
+++ src/Html/Article.hs
@@ -26,8 +26,10 @@ articleHtml = do
       meta_ [httpEquiv_ "x-ua-compatible", content_ "ie=edge"]
       meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
       meta_ [name_ "twitter:card", content_ "summary"]
-      meta_ [name_ "twitter:site", content_ "@clock_void"]
-      meta_ [name_ "twitter:title", content_ "$title$"]
+      meta_ [name_ "og:site", content_ "@clock_void"]
+      meta_ [name_ "og:title", content_ "$title$"]
+      meta_ [name_ "og:image", content_ imgClockvoid]
+      meta_ [name_ "og:type", content_ "article"]
       link_ [rel_ "stylesheet", href_ "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/dracula.min.css"]
       script_ [src_ "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js"] empty
       script_ "hljs.initHighlightingOnLoad();"
