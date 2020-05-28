1cb9a0f379a78fbe4e2916b4266e9d7c3965f4e8
clockvoid <hidenari.t1997nov@gmail.com>
2020-05-28 12:03:33 +0900

    add: twitter share button

--- src/Html/BlogPost.hs
+++ src/Html/BlogPost.hs
@@ -16,6 +16,7 @@ blogPostPath = "templates/blog-post.html"
 blogPostHtml :: Html ()
 blogPostHtml = do
   h1_ [class_ "title"] "$title$"
+  a_ [class_ "twitter-share-button", href_ "https://twitter.com/intent/tweet?text=$title$&via=clock_void"] "Tweet"
   p_ [class_ "date"] "Posted on $date$ $if(updated-date)$ , Updated on $updated-date$ $endif$"
   p_ [style_ "margin-bottom: 1.5rem"] "$if(summary)$ $summary$ $endif$"
   article_ [class_ "content"] "$body$"
