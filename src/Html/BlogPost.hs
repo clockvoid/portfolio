c21ba912b925ab4e98f87e76ee19df8897400ba3
clockvoid <hidenari.t1997nov@gmail.com>
2020-06-08 23:15:38 +0900

    add: disqus form

--- src/Html/BlogPost.hs
+++ src/Html/BlogPost.hs
@@ -20,6 +20,9 @@ blogPostHtml = do
   p_ [class_ "date"] "Posted on $date$ $if(updated-date)$ , Updated on $updated-date$ $endif$"
   p_ [style_ "margin-bottom: 1.5rem"] "$if(summary)$ $summary$ $endif$"
   article_ [class_ "content"] "$body$"
-
-
+  div_ [id_ "disqus_thread"] ""
+  script_ "(function() { var d = document, s = d.createElement('script');s.src = 'https://clockvoid-tk.disqus.com/embed.js';s.setAttribute('data-timestamp', +new Date());(d.head || d.body).appendChild(s);})();"
+  noscript_ $
+    "Please enable JavaScript to view the"
+    --a_ [href_ "http://disqus.com/?ref_noscript"] "comments powered by Disqus."
   
